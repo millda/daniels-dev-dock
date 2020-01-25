6e961979bd9affea039602383d226509c9ba54e2
millda <miller0daniel@gmail.com>
2020-01-25 13:03:57 -0800

    it's working :raised_hands:

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 if [ ! -d $HOME/developement_backup ]; then
   mkdir $HOME/development_backup
 fi
-docker build -t devenv ~/env
+docker build -t milldr/base:latest ./images/base  
+docker build -t milldr/aws-developer:latest ./images/aws-developer
 docker run -it --rm \
   -v $HOME/.gitconfig:/root/.gitconfig \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v $HOME/.ssh/:/root/.ssh/ \
   -v $HOME/development_backup:/home/developer/shared \
-  devenv zsh
+  milldr/aws-developer zsh
