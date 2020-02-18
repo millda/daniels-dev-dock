d0ab0e2d28655f064937e3e7af9dd0f469afe2fb
Daniel Miller <daniel.miller@stelligent.com>
2020-02-17 19:09:29 -0800

    created image for go dev, moved shared depends to base

---
+++ bin/start-aws-dev.sh
@@ -0,0 +1,11 @@
+if [ ! -d $HOME/development_backup ]; then
+  mkdir $HOME/development_backup
+fi
+docker build -t milldr/base:latest ./images/base &&
+docker build -t milldr/aws-developer:latest ./images/aws-developer &&
+docker run -it --rm \
+  -v $HOME/.gitconfig:/root/.gitconfig \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v $HOME/.ssh/:/root/.ssh/ \
+  -v $HOME/development_backup:/home/developer/shared \
+  milldr/aws-developer zsh
--- bin/start-go-dev.sh
+++ bin/start-go-dev.sh
@@ -1,11 +1,11 @@
 if [ ! -d $HOME/development_backup ]; then
   mkdir $HOME/development_backup
 fi
-docker build -t milldr/base:latest ./images/base  
-docker build -t milldr/aws-developer:latest ./images/aws-developer
+docker build -t milldr/base:latest ./images/base  &&
+docker build -t milldr/go-developer:latest ./images/go-developer &&
 docker run -it --rm \
   -v $HOME/.gitconfig:/root/.gitconfig \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v $HOME/.ssh/:/root/.ssh/ \
   -v $HOME/development_backup:/home/developer/shared \
-  milldr/aws-developer zsh
+  milldr/go-developer zsh
