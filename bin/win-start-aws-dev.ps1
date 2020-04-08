8d069e79b3abf34fcf8a068b17bd9b4ce96b0b15
milldr <miller0daniel@gmail.com>
2020-04-07 21:31:54 -0700

    install homebrew in aws-dev, add windows support

--- bin/win-start-aws-dev.ps1
+++ bin/win-start-aws-dev.ps1
@@ -2,10 +2,9 @@
 
 $Service = Test-Path $HOME/development_backup
 if (-Not $Service) {New-Item -Path "$HOME" -Name "development_backup" -ItemType "directory"}
-docker build -t milldr/web-developer:latest ./images/web-developer
+docker build -t milldr/aws-developer:latest ./images/aws-developer
 docker run -it --rm `
   -v $HOME/.gitconfig:/home/developer/.gitconfig `
   -v $HOME/.ssh/:/home/developer/.ssh/ `
   -v $HOME/development_backup:/home/developer/shared `
-  -p 1313:1313 `
-  milldr/web-developer zsh
+  milldr/aws-developer zsh
