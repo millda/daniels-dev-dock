3f39f554e4cdc5e254ccbc7d13582b670731001b
Dan Miller <miller0daniel@gmail.com>
2021-06-14 10:25:36 -0700

    updated versions

--- bin/win-start-complete-toolset.ps1
+++ bin/win-start-complete-toolset.ps1
@@ -1,8 +1,8 @@
 $DIR=$Env:DEV
 $Service = Test-Path $DIR/development_backup
 if (-Not $Service) {New-Item -Path "$DIR" -Name "development_backup" -ItemType "directory"}
-docker build -t milldr/base:latest ./images/base &&
-docker build -t milldr/complete-devops-toolset:latest ./images/complete-devops-toolset &&
+docker build -t milldr/base:latest ./images/base
+docker build -t milldr/complete-devops-toolset:latest ./images/complete-devops-toolset
 docker run -it --rm `
   -v $DIR/.ssh/:/home/developer/.ssh/ `
   -v $DIR/.aws/:/home/developer/.aws/ `
