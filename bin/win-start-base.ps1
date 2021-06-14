cc6e56239c4e1ebd8547ddd05817890e912db7c9
Dan Miller <miller0daniel@gmail.com>
2021-06-14 10:00:02 -0700

    corrected issues with base

--- bin/win-start-base.ps1
+++ bin/win-start-base.ps1
@@ -1,7 +1,7 @@
 $DIR=$Env:DEV
 $Service = Test-Path $DIR/development_backup
 if (-Not $Service) {New-Item -Path "$DIR" -Name "development_backup" -ItemType "directory"}
-docker build -t milldr/base:latest ./images/base &&
+docker build -t milldr/base:latest ./images/base
 docker run -it --rm `
   -v $DIR/.ssh/:/home/developer/.ssh/ `
   -v $DIR/.aws/:/home/developer/.aws/ `
